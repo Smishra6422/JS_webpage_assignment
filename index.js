5a20304d58c7cfdb21a9b21a154a9f633bfcb342
Shubham <Ks6422@hotmail.com>
2020-08-18 00:46:41 -0400

    all the functionality like click and search working well

---
+++ index.js
@@ -0,0 +1,192 @@
+const cardDetail = {
+  shirts: [
+    {
+      name: "Blue shirts",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/6591210/2018/6/13/6d6d0cce-f1c8-467a-8eb5-3ed886c001e01528882366244-Mast--Harbour-Men-Blue-Slim-Fit-Solid-Casual-Shirt-421152888-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Green shirts",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11758172/2020/8/13/688bf131-878f-4f2f-aaa3-55fceb2f9e931597317591413-HERENOW-Men-Green-Slim-Fit-Solid-Casual-Shirt-70315973175895-1.jpg",
+      price: 10,
+    },
+    {
+      name: "White shirts",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1830428/2017/3/30/11490857788324-Hancock-Men-Shirts-3661490857788137-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Black shirts",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10906886/2019/11/23/a562e167-4996-4f76-b194-fa52a1f0e25e1574505900989-IVOC-Mens-Solid-Shirt-9151574505899261-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Green and Navy Blue shirts",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11957980/2020/7/15/62f149fe-4bd3-4bef-b0a4-c3e40c92caaa1594819839152JeansRoadsterMenJeansRoadsterMenJeansRoadsterMenShirtsRoadst1.jpg",
+      price: 10,
+    },
+    {
+      name: "Regular Fit shirts",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11948736/2020/7/13/f40ae7fc-f710-4511-bfb7-b99e8843ac041594624107672-Mast--Harbour-Men-Shirts-3301594624105396-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Jack & Jones shirts",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10980350/2020/2/4/b28c1fc7-a2c3-4c09-9f8a-1a7d95519cca1580820941715-Jack--Jones-Men-Shirts-7961580820940060-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Denim shirts",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/4629613/2020/3/24/33cfb138-ccd1-4667-ab84-2fba6b83fcaf1585049054571-Ecko-Unltd-Men-Blue-Slim-Fit-Solid-Casual-Shirt-402158504905-1.jpg",
+      price: 10,
+    },
+  ],
+  tshirt: [
+    {
+      name: "Yellow Hrx T-shirt",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2019/6/8/972c9498-3a37-4d5d-976c-4493b4d5c0021559989322793-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Round-Neck-T-Shirt--1.jpg",
+      price: 10,
+    },
+    {
+      name: "Light Green T-shirt",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11077774/2019/12/24/29bdd2c8-3e2f-43aa-8ce5-55f3a501a4fa1577166621272-Difference-of-Opinion-Men-Sea-Green-Solid-Round-Neck-T-shirt-1.jpg",
+      price: 10,
+    },
+    {
+      name: "White T-shirt",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2020/7/24/27a3b1b3-8575-4cf7-aa51-f7ce0448905a1595542208222-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Black T-shirt",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11241802/2020/7/15/fd72c906-9709-428f-ba3f-d9faed2e7f381594797007269-Kook-N-Keech-Marvel-Men-Black-Printed-Round-Neck-T-shirt-585-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Red Puma T-shirt",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10994130/2019/12/9/e55f0f7c-b599-4673-811b-53f133b3ce171575884083534-Puma-Men-Tshirts-9501575884081629-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Navy Blue T-shirt",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11630450/2020/6/16/886b3a4c-e3cb-4a0d-9234-2a158bdb127a1592299314119-Nautica-Men-Tshirts-4291592299312595-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Red Allen T-shirt",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10976230/2019/11/19/734fbc1b-99ba-4a69-a064-b6864a0bf66b1574150698771-Allen-Solly-Casual-Polo-Tshirt-4261574150697199-1.jpg",
+      price: 10,
+    },
+  ],
+  jeans: [
+    {
+      name: "HERE&NOW Jeans",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2272201/2018/2/8/11518074454367-HERENOW-Men-Black-Tapered-Fit-Mid-Rise-Stretchable-Jeans-6121518074454181-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Highlander Jeans",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2019/2/2/d396ec1f-84c4-455d-b219-f4b0a11700d61549130797254-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Moda Rapido Jeans",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11159402/2020/8/5/3094186e-712f-4b4d-9215-f1b1515621cc1596620984968-Moda-Rapido-Men-Jeans-271596620982952-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Roadster",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11274548/2020/8/4/bbc5a1dd-ba40-43b7-8a2f-de7d25fbd38d1596533902419-Roadster-Men-Jeans-9271596533900219-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Wrogn Jeans",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11359218/2020/8/3/904ee46e-2313-461e-9e71-99e9faf2ce521596457234576-WROGN-Men-Jeans-6021596457232039-1.jpg",
+      price: 10,
+    },
+    {
+      name: "Flying Machine",
+      imageUrl:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10819694/2019/11/15/6c2f3a77-cd28-49bb-be80-762d77fce54e1573819805242-Flying-Machine-Men-Jeans-5821573819802855-1.jpg",
+      price: 10,
+    },
+  ],
+};
+
+let imageContainer = document.getElementsByClassName("image-container")[0];
+let buttons = document.getElementById("buttons");
+let serachBar = document.getElementById("exampleFormControlInput1");
+let cards = document.getElementsByClassName("card");
+
+function putImages(category) {
+  for (let i = 0; i < category.length; i++) {
+    let card = `<div class="card" >
+          <img src=${category[i].imageUrl} class="card-img-top" alt="..." />
+          <div class="card-body">
+            <span class="card-text">
+              ${category[i].name}
+            </span>
+            <span>$ ${category[i].price}</span>
+          </div>
+        </div>`;
+
+    imageContainer.insertAdjacentHTML("beforeend", card);
+  }
+}
+
+putImages(cardDetail.shirts);
+
+buttons.addEventListener("click", (e) => {
+  let categoryName = e.target.dataset.categoryValue;
+  if (categoryName) {
+    imageContainer.innerHTML = "";
+    putImages(cardDetail[categoryName]);
+  }
+});
+
+serachBar.addEventListener("keyup", (e) => {
+  // // Check it later either to keep in final one or not
+  if (e.target.value.length === 1) {
+    document.body.style.background = "#2C3335";
+  } else if (e.target.value.length % 2 === 0) {
+    document.body.style.background = "#192A56";
+  } else if (e.target.value.length % 3 === 0) {
+    document.body.style.background = "#4C4B4B";
+  } else if (e.target.value.length % 5 === 0) {
+    document.body.style.background = "#01CBC6";
+  } else {
+    document.body.style.background = "#1287A5";
+  }
+
+  const searchInputValue = e.target.value.toLowerCase();
+
+  Array.from(cards).forEach((card) => {
+    let cardName = card.children[1].firstElementChild.textContent;
+    console.log(cards);
+    if (cardName.toLowerCase().includes(searchInputValue)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
